f53c86327bdb74f3e04500275a143e2f18460500
YeSeul-0w0 <choi333ys@gmail.com>
2024-02-25 20:40:29 +0900

    style: update style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Router from "./component/Router";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  return <Router />;
+  return (
+    <ChakraProvider>
+      <Router />
+    </ChakraProvider>
+  );
 }
 
 export default App;
--- src/component/Main/MainScreen.js
+++ src/component/Main/MainScreen.js
@@ -1,21 +1,59 @@
 import { useNavigate } from "react-router-dom";
+import { Heading } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import React from "react";
+import dummy from "../../static/cafe_list.json"; // 더미 데이터
 function MainScreen() {
   const navigate = useNavigate();
   const goCalendar = () => {
     navigate("/calendar");
   };
+  const today = new Date();
+
   return (
     <div>
-      test
+      <Heading as="h2" size="xl">
+        {today.getMonth() + 1}월
+      </Heading>
+      <TableContainer>
+        <Table size="sm">
+          <Thead>
+            <Tr>
+              <Th>Cafe Name</Th>
+              <Th>Date</Th>
+              <Th>Location</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {dummy.cafes.map((cafe) => (
+              <Tr key={cafe.key}>
+                <Td>{cafe.name}</Td>
+                <Td>{cafe.date}</Td>
+                <Td>{cafe.location}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
       <br />
-      <button
+      <Button
+        colorScheme="teal"
+        size="xs"
         onClick={() => {
           goCalendar();
         }}
       >
-        {" "}
         see the calendar
-      </button>
+      </Button>
     </div>
   );
 }
