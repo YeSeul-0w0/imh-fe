5e20cf14e3ff57896fcdb13ed03102dda5b6d440
YeSeul-0w0 <choi333ys@gmail.com>
2024-02-25 20:39:51 +0900

    feat: add chakra-ui

--- src/component/Router.js
+++ src/component/Router.js
@@ -2,16 +2,52 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import MainScreen from "./Main/MainScreen";
 import Calendar from "./Main/Calendar";
+import styled from "styled-components";
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainScreen />} />
-        <Route path="/calendar" element={<Calendar />} />
-      </Routes>
-    </BrowserRouter>
+    <AppLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainScreen />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
+      </BrowserRouter>
+    </AppLayout>
   );
 }
+
+const AppLayout = styled.div`
+  max-width: 500px;
+  height: 98vh;
+  // min-height: 100vh;
+  margin: auto;
+  border: 1px solid lightgrey;
+  padding: 2%;
+  //display: flex;
+  //flex-flow: column nowrap;
+  //align-items: center;
+  //align-content: stretch;
+  position: relative;
+  transform: translateX(-0%);
+  /* overflow-y: hidden; */
+  overflow-x: hidden;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  ::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+  @media screen and (min-height: 901px) {
+    min-height: unset;
+    max-height: 98vh;
+    top: 50%;
+    left: 50%;
+    padding: 2%;
+    transform: translate(-50%, -50%);
+    margin: unset;
+    position: absolute;
+    width: 100%;
+  }
+`;
 
 export default Router;
