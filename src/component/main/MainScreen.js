a7db385973c1c516f401085c4b887bb368903ef1
YeSeul-0w0 <choi333ys@gmail.com>
2024-03-05 22:16:01 +0900

    docs: make header

--- src/component/main/MainScreen.js
+++ src/component/main/MainScreen.js
@@ -1,26 +1,38 @@
-import { useNavigate } from "react-router-dom";
-import { Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, IconButton } from "@chakra-ui/react";
 import React from "react";
 import dummy from "../../static/cafe_list.json"; // 더미 데이터
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import Calendar from "./Calendar";
 import List from "./List";
+import { AddIcon } from "@chakra-ui/icons";
 function MainScreen() {
   const today = new Date();
 
   return (
     <div>
-      <Heading as="h2" size="xl">
-        {today.getMonth() + 1}월
-      </Heading>
+      <Box display="grid" gridGap={2} gridAutoFlow="column" margin="2%">
+        <Heading as="h2" size="xl">
+          {today.getMonth() + 1}월
+        </Heading>
+        <Flex justifyContent="flex-end">
+          <IconButton
+            isRound={true}
+            size="sm"
+            variant="outline"
+            colorScheme="teal"
+            icon={<AddIcon />}
+          />
+        </Flex>
+      </Box>
       <Tabs isFitted variant="enclosed">
         <TabList>
           <Tab>List</Tab>
+
           <Tab>Calendar</Tab>
         </TabList>
 
         <TabPanels>
-          <TabPanel>
+          <TabPanel style={{ height: "100vh" }}>
             <List cafes={dummy} />
           </TabPanel>
           <TabPanel>
