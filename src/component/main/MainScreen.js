adfb50d49b67284e95ab0d0f797e1deb930444e5
YeSeul-0w0 <choi333ys@gmail.com>
2024-03-06 21:46:01 +0900

    feat: add modal for register cafe data

--- src/component/main/MainScreen.js
+++ src/component/main/MainScreen.js
@@ -1,21 +1,33 @@
-import { Box, Flex, Heading, IconButton } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 import dummy from "../../static/cafe_list.json"; // 더미 데이터
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import Calendar from "./Calendar";
 import List from "./List";
 import { AddIcon } from "@chakra-ui/icons";
+import AddModal from "./AddModal";
+
 function MainScreen() {
   const today = new Date();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <div>
+      {isOpen ? <AddModal isOpen={isOpen} onClose={onClose} /> : null}
       <Box display="grid" gridGap={2} gridAutoFlow="column" margin="2%">
         <Heading as="h2" size="xl">
           {today.getMonth() + 1}월
         </Heading>
         <Flex justifyContent="flex-end">
           <IconButton
+            id="main-screen-add"
+            onClick={onOpen}
             isRound={true}
             size="sm"
             variant="outline"
@@ -27,7 +39,6 @@ function MainScreen() {
       <Tabs isFitted variant="enclosed">
         <TabList>
           <Tab>List</Tab>
-
           <Tab>Calendar</Tab>
         </TabList>
 
@@ -40,6 +51,7 @@ function MainScreen() {
           </TabPanel>
         </TabPanels>
       </Tabs>
+      <AddModal />
     </div>
   );
 }
